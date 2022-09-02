40211608f105cf9547b38127eb49ed9a6bd143ea
wangjunjie1 <wangjunjie1@360.cn>
2022-09-02 18:33:26 +0800

    添加说明和代码

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wallpaper
-获取壁纸
+## _目录说明_
+- bing 获取bing每日图片
+
+
+## 介绍
+项目通过调用官方相关接口获取4k图片
+
+执行程序下载今日bing的背景图片，默认图片会下载到当前目录 pic目录下。
+下载完成后会将图片名称和copayrigt信息写入到introduction.txt文件中。
+
+
+## 使用
+linux:
+```sh
+bing -download <your download dir> -group n
+```
+| Param | Type |README |
+| ------ | ------ | ------ |
+| -download |string| 参数不指定图片将下载到当前目录下的 pic目录中 |
+| -group |y/n| 默认值为n，图片保存在当前目录下。y：图片会在当前目录下按月建立文件夹分组 |
+
---
+++ bing/main.go
@@ -0,0 +1,125 @@
+// 获取bing 4k 高清壁纸
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+var baseDomain = "https://cn.bing.com/"
+
+type bingResponse struct {
+	Images []BingImage `json:"images"`
+}
+
+// BingImage 图片信息
+type BingImage struct {
+	Startdate     string        `json:"startdate"`
+	Fullstartdate string        `json:"fullstartdate"`
+	Enddate       string        `json:"enddate"`
+	URL           string        `json:"url"`
+	Urlbase       string        `json:"urlbase"`
+	Copyright     string        `json:"copyright"`
+	Copyrightlink string        `json:"copyrightlink"`
+	Title         string        `json:"title"`
+	Quiz          string        `json:"quiz"`
+	Wp            bool          `json:"wp"`
+	Hsh           string        `json:"hsh"`
+	Drk           int           `json:"drk"`
+	Top           int           `json:"top"`
+	Bot           int           `json:"bot"`
+	Hs            []interface{} `json:"hs"`
+}
+
+// https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160
+func main() {
+	var savePicDir string
+	flag.StringVar(&savePicDir, "p", "./pic", "文件夹路径")
+	flag.Parse()
+
+	_, err := os.Stat(savePicDir)
+	if os.IsNotExist(err) {
+		os.MkdirAll(savePicDir, 0666)
+	}
+
+	image, _ := getImageInfo()
+	saveImage(image, savePicDir)
+}
+
+func getImageInfo() (*BingImage, error) {
+	picNum := 1 //获取图片数
+	nc := time.Now().UnixNano()
+	baseURI := fmt.Sprintf("/HPImageArchive.aspx?format=js&idx=0&n=%d&nc=%d&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160", picNum, nc)
+	reqURL := fmt.Sprintf("%s%s", baseDomain, baseURI)
+
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	//
+	var bingResp bingResponse
+	if err := json.Unmarshal(body, &bingResp); err != nil {
+		return nil, err
+	}
+	if len(bingResp.Images) > 1 {
+		return nil, fmt.Errorf("获取图片数量错误")
+	}
+	return &bingResp.Images[0], nil
+}
+
+func saveImage(img *BingImage, savePicDir string) {
+	imgURL := fmt.Sprintf("%s%s", baseDomain, img.URL)
+	u, err := url.Parse(img.URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	q := u.Query()
+
+	respImg, err := http.Get(imgURL)
+	imgFilePath := fmt.Sprintf("%s/%s", savePicDir, q.Get("id"))
+	_, err = os.Stat(imgFilePath)
+	if !os.IsNotExist(err) {
+		fmt.Println("文件已存在")
+		return
+	}
+
+	imageFd, err := os.Create(imgFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer imageFd.Close()
+	_, err = io.Copy(imageFd, respImg.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//
+	copyrightInfoPath := fmt.Sprintf("%s/introduction.txt", savePicDir)
+	content := fmt.Sprintf("%s\t%s\t%s\n", img.Startdate, q.Get("id"), img.Copyright)
+	writeFile(copyrightInfoPath, []byte(content))
+}
+
+func writeFile(filename string, data []byte) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	if err == nil && n < len(data) {
+		err = io.ErrShortWrite
+	}
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+	return err
+}
